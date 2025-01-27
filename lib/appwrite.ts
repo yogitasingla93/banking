b78d438eecf1c40182f7d1999996b2781e65d850
Yogita Singla <yogitasingla93@gmail.com>
2025-01-27 17:31:21 +0530

    adding await to cookie update

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -8,7 +8,8 @@ export async function createSessionClient() {
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
     .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
 
-  const session = cookies().get("appwrite-session");
+  const session = await cookies().get("appwrite-session");
+
 
   if (!session || !session.value) {
     throw new Error("No session");
